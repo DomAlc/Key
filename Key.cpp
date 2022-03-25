c43547c4dd1bfc34bc9d5f631bd10e9c3eae5302
DomAlc <32032722+DomAlc@users.noreply.github.com>
2022-03-25 22:26:13 +0100

    Add files via upload

---
+++ Key.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include "Key.h"
+
+// class constructor
+
+Key::Key(int pin, bool setPullUp) {
+      pinNumber = pin;
+      if (setPullUp) {
+        pinMode(pin, INPUT_PULLUP);
+      }
+      else {
+        pinMode(pin, INPUT);
+      }
+      keyState = false;
+      keyOldState = false;
+      keySwitch = false;
+      keyPE = false;
+      keyNE = false;
+}
+
+Key::update() {
+      if ((!digitalRead(pinNumber) != keyState) && !keySwitch) {
+        timeStamp = millis();
+        keySwitch = true;
+      }
+
+      if (keySwitch && ((millis() - timeStamp) > 50)) {
+        keyState = !digitalRead(pinNumber);
+        keySwitch = false;
+      }
+
+      keyPE = (!keyOldState & keyState);
+      keyNE = (keyOldState & !keyState);
+      keyOldState = keyState;
+} // end update
+
+bool Key::getState() {
+      return keyState;
+}
+
+bool Key::getPE() {
+      return keyPE;
+}
+
+bool Key::getNE() {
+      return keyNE;
+}
---
+++ Key.h
@@ -0,0 +1,46 @@
+/*****************************************************************************
+ * Key.h *
+ *****************************************************************************
+ * allows easy handling of keys with debounce protection and edge detection  *
+ *                                                                           *
+ * v1.0 - Domingo Alcala  domingo.alcala@gmail.com                           *
+ *****************************************************************************
+ */
+
+#ifndef Key_h
+#define Key_h
+
+class Key {
+
+  private:
+    int pinNumber;
+    bool keyState;
+    bool keySwitch;
+    bool keyOldState;
+    bool keyPE;
+    bool keyNE;
+    long timeStamp;
+
+  public:
+
+    /* class constructor */
+    /*   pin : the pin number where the key is connected  */
+    /*   setPullUp : true to set internal pullup resistors */
+
+    Key(int pin, bool setPullUp);
+
+    /* update must be called every loop */
+    update();
+
+    /* return key state: true = key pressed */
+    bool getState();
+
+    /* return true if positive edge detected. Will remain true until next update */
+    bool getPE() ;
+
+    /* return true if negative edge detected. Will remain true until next update */
+    bool getNE();
+};
+
+#endif
+
